5dcfc85a98587a9cc20b1fa476718e34ac9cfa43
Jihun Kim <devgrapher@gmail.com>
2022-02-08 11:19:36 +0900

    aoc2018 day 1

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -1,16 +1,27 @@
-(ns aoc2018-1)
+(ns aoc2018-1 (:require clojure.string))
+
+(def input "./resources/aoc2018_1.txt")
 
 ;; 파트 1
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
+(def numbers (-> input
+                 slurp
+                 clojure.string/split-lines
+                 ((fn [n] (map #(Integer. %) n)))))
 
+(def part1 (reduce + numbers))
 
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
 
-
+(def part2
+  (->> (reductions + (cycle numbers))
+       (reduce (fn [sum n] (if (contains? sum n)
+                              (reduced n)
+                              (conj sum n))) #{0})))
 
 ;; #################################
 ;; ###        Refactoring        ###
