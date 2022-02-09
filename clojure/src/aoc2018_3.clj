8a8895828c4cb3498626f4c5c6d63714918bc208
Jihun Kim <devgrapher@gmail.com>
2022-02-09 13:22:32 +0900

    aoc 2018 day3

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,5 +1,18 @@
-(ns aoc2018_3)
+(ns aoc2018-3
+  (:require clojure.set
+            clojure.string
+            [clojure.java.io :as io]))
 
+(def input (-> (io/resource "aoc2018_3.txt")
+               slurp
+               clojure.string/split-lines))
+
+(defn parse-claim [claim]
+  (let [[_ & groups] (re-matches #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)" claim)
+        [id x y w h] (map read-string groups)]
+    {:id id :x x :y y :w w :h h}))
+
+(def claims (map parse-claim input))
 
 ;; 파트 1
 ;; 다음과 같은 입력이 주어짐.
@@ -23,10 +36,64 @@
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
 
+(defn gen-rect
+  ([{:keys [x y w h]}] (gen-rect [x y] [w h]))
+  ([[x y] [w h]]
+   (into #{}
+         (for [i (range w)
+               j (range h)]
+           [(+ x i) (+ y j)]))))
+
+(def claimed-rects (map gen-rect claims))
+
+(defn frequencies-through [coll]
+  (->> coll
+       (map frequencies)
+       (apply merge-with +)))
+
+(def point-frequencies (frequencies-through claimed-rects))
+
+(def part1
+  (count
+   (filter #(> (val %) 1) point-frequencies)))
 
 
+(comment
+  (parse-claim "#144 @ 208,134: 3x10")
+  (gen-rect '(:x 208 :y 134 :w 3 :h 10))
+
+  (clojure.set/union (gen-rect [1 3] [4 4])
+                     (gen-rect [3 1] [4 4])
+                     (gen-rect [5 5] [2 2]))
+  (count
+   (filter #(> (val %) 1) (frequencies-through [(gen-rect [1 3] [4 4])
+                                                (gen-rect [3 1] [4 4])
+                                                (gen-rect [5 5] [2 2])]))))
 
 ;; 파트 2
 ;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
 ;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
 ;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
+
+(def rects-by-id
+  (into {} (map vector (map :id claims) (map gen-rect claims))))
+
+(defn find-frequencies [rect]
+  (map point-frequencies rect))
+
+(defn every-one? [coll]
+  (every? #(= 1 %) coll))
+
+(def part2 (reduce (fn [_ v]
+                     (let [id (key v)
+                           points (val v)
+                           freq (find-frequencies points)]
+                       (when (every-one? freq)
+                         (reduced id))))
+                   rects-by-id))
+
+(comment
+  (find-frequencies (gen-rect '(:x 208 :y 134 :w 3 :h 10)))
+  (find-frequencies (gen-rect [1 3] [4 4]))
+
+  )
