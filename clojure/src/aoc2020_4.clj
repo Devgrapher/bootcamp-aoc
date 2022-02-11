a18345c1a4c38b071b6ac00f5676df3a86387813
Jihun Kim <devgrapher@gmail.com>
2022-02-11 16:22:00 +0900

    aoc 2020 day 4

---
+++ clojure/src/aoc2020_4.clj
@@ -0,0 +1,66 @@
+(ns aoc2020-4
+  (:require [clojure.spec.alpha :as spec]
+            [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(def input (io/resource "aoc2020_4.txt"))
+
+(defn entry->fields
+  "패스포트 입력을 각 필드의 kv로 변환합니다."
+  [entry]
+  (->> (str/split entry #" ")
+       (map #(let [[left right] (str/split % #":")]
+               [(keyword left) right]))))
+
+(defn parse-input
+  "입력을 패스포트 맵의 리스트로 변환합니다."
+  [resource]
+  (let [entries (-> resource
+                    slurp
+                    (str/replace #"\n" " ")
+                    (str/split #"  "))]
+    (->> entries
+         (map entry->fields)
+         (map #(into {} %)))))
+
+
+;;; part1
+
+(spec/def :passport/simple #(every? % #{:hcl :hgt :ecl :iyr :pid :byr :eyr}))
+
+(defn part1 []
+  (->> (parse-input input)
+       (filter #(spec/valid? :passport/simple %))
+       count))
+
+;;; part2
+
+(spec/def :passport/ecl #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"})
+(spec/def :passport/pid #(boolean (re-find #"^\d{9}$" %)))
+(spec/def :passport/eyr #(spec/int-in-range? 2020 2031 (Integer. %)))
+(spec/def :passport/hcl #(boolean (re-find #"^#[\w\d]{6}$" %)))
+(spec/def :passport/byr #(spec/int-in-range? 1920 2003 (Integer. %)))
+(spec/def :passport/iyr #(spec/int-in-range? 2010 2021 (Integer. %)))
+(spec/def :passport/hgt #(boolean (re-find #"^\d{3}cm$|^\d{2}in$" %)))
+(spec/def :passport/cid some?)
+
+(spec/def :passport/passport
+  (spec/keys :req-un [:passport/ecl
+                      :passport/pid
+                      :passport/eyr
+                      :passport/hcl
+                      :passport/byr
+                      :passport/iyr
+                      :passport/hgt]
+             :opt-un [:passport/cid]))
+
+(defn part2 []
+  (->> (parse-input input)
+       (filter #(spec/valid? :passport/passport %))
+       count))
+
+(comment
+  (parse-input input)
+
+  (spec/explain :passport/passport {:ecl "gry" :pid 860033327 :eyr 2020 :hcl "#fffffd"
+                                    :byr 1937 :iyr 2017 :cid 147 :hgt "183cm"}))
