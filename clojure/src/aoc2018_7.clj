68d4a30b642b59cedbc85712c7d78327dc351e1b
Jihun Kim <devgrapher@gmail.com>
2022-02-15 09:12:14 +0900

    aoc 2018 day 7 - part 1

--- clojure/src/aoc2018_7.clj
+++ clojure/src/aoc2018_7.clj
@@ -5,73 +5,64 @@
 
 (def line-re #"Step (\w+) must be finished before step (\w+) can begin.")
 
-(defn read-input [] (-> (io/resource "aoc2018_7.txt")
-                        slurp
-                        str/split-lines))
+(defn read-input [resource] (-> (io/resource resource)
+                                slurp
+                                str/split-lines))
 
-(defn parse-input [lines]
+(defn parse-input
+  "입력에서 의존성에 따라 {A #{B C}}의 맵을 만듭니다."
+  [lines]
   (->> lines
        (map #(re-matches line-re %))
        (map rest)
-       #_(map (fn [[left right]] (list
-                                (int (first (seq left)))
-                                (int (first (seq right))))))))
+       (reduce (fn [acc [left right]]
+                 (update
+                  acc
+                  left
+                  #(if % (conj % right) #{right})))
+               {})))
 
 ;; part 1
-
-(defn adder
-  [acc nodes]
-  (loop [acc acc
-         nodes nodes
-         len (count nodes)]
-    (if (= (count acc) len)
-      acc
-      (recur (reduce (fn [acc [left right]]
-                       (update acc left #(conj % right)))
-                     acc
-                     nodes)
-             nodes
-             len))))
-
 (defn find-entries
   [deps]
-  (let [lefts (into #{} (keys deps))
+  (let [lefts (into (sorted-set) (keys deps))
         rights (apply clojure.set/union (vals deps))]
-    (->> lefts
-         (filter #(not (rights %)))
-         sort)))
+    (filter #(not (rights %)) lefts)))
+
+(defn find-last-jobs
+  "다른 작업들이 의존하지 않는 마지막 작업들을 찾습니다."
+  [deps]
+  (clojure.set/difference
+   (apply clojure.set/union (vals deps))
+   (into (sorted-set) (keys deps))))
+
+(defn find-orders
+  "{C #{F A}, A #{B D}, ...} -> [C A ...]"
+  [deps]
+  (loop [{:keys [remaining
+                 done
+                 entries]} {:remaining deps
+                            :done []
+                            :entries (find-entries deps)}]
+    (prn remaining done entries)
+    (if (empty? remaining)
+      (apply conj done (find-last-jobs deps))
+      (let [entry (first entries)
+            rem (dissoc remaining entry)]
+        (recur {:remaining rem
+                :done (conj done entry)
+                :entries (find-entries rem)})))))
+
+(defn part1
+  []
+  (let [orders (-> (read-input "aoc2018_7.txt")
+                   parse-input
+                   find-orders)]
+    (apply str orders)))
+
+;; part 2
+
 
 (comment
-  (let [input (parse-input (read-input))
-        deps (reduce (fn [acc [left right]]
-                       (update
-                        acc
-                        left
-                        #(if % (conj % right) #{right})))
-                     {} input)
-        lasts (sort (clojure.set/difference
-               (apply clojure.set/union (vals deps))
-               (into #{} (keys deps))))]
-    (loop [{:keys [remaining
-                   done
-                   entries] :as acc} {:remaining deps
-                                      :done []
-                                      :entries (find-entries deps)}]
-      (println acc)
-      (if (empty? remaining)
-        (apply conj done lasts)
-        (recur (reduce (fn [{:keys [remaining done]}
-                            entry]
-                         {:remaining (dissoc remaining entry)
-                          :done (conj done entry)
-                          :entries (find-entries (dissoc remaining entry))})
-                 acc
-                 entries)))))
 
-  (parse-input (read-input))
-  (zipmap (distinct (flatten (parse-input (read-input)))) (repeat nil))
-  )
-
-(comment
-  )
-
+  (parse-input (read-input "aoc2018_7.txt")))
