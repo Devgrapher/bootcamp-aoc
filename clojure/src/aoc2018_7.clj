5f6ecce642930f6e84fb1a3cd774f4167aff96d5
Jihun Kim <devgrapher@gmail.com>
2022-02-16 09:00:18 +0900

    aoc 2018 day 7 part2

--- clojure/src/aoc2018_7.clj
+++ clojure/src/aoc2018_7.clj
@@ -5,64 +5,126 @@
 
 (def line-re #"Step (\w+) must be finished before step (\w+) can begin.")
 
-(defn read-input [resource] (-> (io/resource resource)
-                                slurp
-                                str/split-lines))
+(def input (-> (io/resource "aoc2018_7.txt")
+               slurp
+               str/split-lines))
 
 (defn parse-input
   "입력에서 의존성에 따라 {A #{B C}}의 맵을 만듭니다."
   [lines]
-  (->> lines
-       (map #(re-matches line-re %))
-       (map rest)
-       (reduce (fn [acc [left right]]
-                 (update
-                  acc
-                  left
-                  #(if % (conj % right) #{right})))
-               {})))
+  (let [pairs (map #(rest (re-matches line-re %)) lines)
+        deps (->> pairs
+                  (map (fn [[left right]] {right #{left}}))
+                  (apply merge-with into))
+        lefts (into #{} (map first pairs))
+        rights (into #{} (map second pairs))
+        entries (filter #(not (rights %)) lefts)]
+    (apply conj deps (map (fn [en] {en #{}}) entries)))
+  )
 
 ;; part 1
-(defn find-entries
-  [deps]
-  (let [lefts (into (sorted-set) (keys deps))
-        rights (apply clojure.set/union (vals deps))]
-    (filter #(not (rights %)) lefts)))
 
-(defn find-last-jobs
-  "다른 작업들이 의존하지 않는 마지막 작업들을 찾습니다."
+(defn find-entries
+  "다른 작업에 의존성이 없는 시작 작업 목록을 반환합니다."
   [deps]
-  (clojure.set/difference
-   (apply clojure.set/union (vals deps))
-   (into (sorted-set) (keys deps))))
+  (keys (filter #(empty? (val % )) deps)))
+
+(defn drop-dep
+  "의존성 목록에서 작업을 제거합니다."
+  [deps job]
+  (let [deps (->> deps
+                  (map (fn [[left right]] {left (disj right job)}))
+                  (into {}))]
+    (dissoc deps job)))
 
 (defn find-orders
   "{C #{F A}, A #{B D}, ...} -> [C A ...]"
   [deps]
-  (loop [{:keys [remaining
-                 done
-                 entries]} {:remaining deps
-                            :done []
-                            :entries (find-entries deps)}]
-    (prn remaining done entries)
-    (if (empty? remaining)
-      (apply conj done (find-last-jobs deps))
-      (let [entry (first entries)
-            rem (dissoc remaining entry)]
-        (recur {:remaining rem
-                :done (conj done entry)
-                :entries (find-entries rem)})))))
+  (loop [{:keys [deps
+                 done]} {:deps deps
+                         :done []}]
+    (prn deps done)
+    (if (empty? deps)
+      done
+      (let [entry (first (sort (find-entries deps)))
+            rem (drop-dep deps entry)]
+        (recur {:deps rem
+                :done (conj done entry)})))))
 
 (defn part1
   []
-  (let [orders (-> (read-input "aoc2018_7.txt")
+  (let [orders (-> input
                    parse-input
                    find-orders)]
     (apply str orders)))
 
 ;; part 2
 
+(def base-time-taking 61)
+(def workers 5)
+
+(defn job-time
+  "작업의 소요시간을 반환합니다."
+  [job]
+  (-> (int (first job))
+      (as-> t (- t (int \A)))
+      (as-> t (+ t base-time-taking))))
+
+(defn alloc-jobs
+  "의존성 맵에서 가용한 작업자수 만큼 꺼냅니다."
+  [deps working]
+  (let [avail-workers (- workers (count working))
+        entries (into #{} (find-entries deps))
+        avail-entries (->> working
+                           (clojure.set/difference entries)
+                           (take avail-workers))]
+    (apply conj working avail-entries)))
+
+(defn assign-time
+  "작업 항목에 작업 소요시간을 쌍으로 반환합니다."
+  [working]
+  (->> working
+       (map (fn [w] {w (job-time w)}))
+       (into {})))
+
+(defn elapse-work
+  "진행중인 작업에 시간을 진척시키고 시간이 소진된 작업은 제거합니다."
+  [work-with-time time-spending]
+  (->> work-with-time
+       (map (fn [[w t]] {w (- t time-spending)}))
+       (filter #(> (val (first %)) 0))
+       (into {})))
+
+(defn do-work
+  "의존성순서에 따라 작업을 진행하고 총 시간을 반환합니다."
+  [deps]
+  (loop [{:keys [deps
+                 work-progressing
+                 time-elaped]} {:deps deps
+                                :work-progressing {} ; {job time-remaining}
+                                :time-elaped 0}]
+    (prn deps work-progressing)
+    (let [time-dt (if (empty? work-progressing) 0 (apply min (map val work-progressing)))
+          time-elaped (+ time-elaped time-dt)
+          work-elapsed (elapse-work work-progressing time-dt)
+          done (remove (set (keys work-elapsed)) (keys work-progressing))
+          deps (reduce (fn [deps job] (drop-dep deps job))
+                       deps
+                       done)]
+      (prn time-dt time-elaped work-elapsed work-elapsed)
+      (if (and (empty? deps) (empty? work-elapsed))
+        time-elaped
+        (let [new-works (-> (alloc-jobs deps (keys work-elapsed))
+                            assign-time)]
+          (recur {:deps deps
+                  :work-progressing (merge new-works work-elapsed)
+                  :time-elaped time-elaped}))))))
+
+(defn part2
+  []
+  (-> (parse-input input)
+      (do-work)))
 
 (comment
-
-  (parse-input (read-input "aoc2018_7.txt")))
+  (job-time "Z")
+  (parse-input input))
