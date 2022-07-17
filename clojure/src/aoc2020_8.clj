eb46c5ea366147a2b8fb239a7167f034bda2de1c
Jihun Kim <jihun.kim@greenlabs.co.kr>
2022-07-17 12:20:27 +0900

    2020 day8

---
+++ clojure/src/aoc2020_8.clj
@@ -0,0 +1,87 @@
+(ns aoc2020-8
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(def input-sample (slurp (io/resource "aoc2020_8_1.txt")))
+(def input (slurp (io/resource "aoc2020_8.txt")))
+
+(defn parse-input [input]
+  (->> input
+      s/split-lines
+      (map #(s/split % #"\s"))
+      (map (fn [[op n]] {:op (keyword op)
+                        :num (Integer. n)}))))
+
+(defn execute [{:keys [instructions pc acc executions]}]
+  (let [{:keys [op num]} (nth instructions pc)
+        executions (conj executions pc)
+        [acc pc] (case op
+                   :acc [(+ acc num) (inc pc)]
+                   :jmp [acc (+ pc num)]
+                   :nop [acc (inc pc)])]
+    {:instructions instructions
+     :pc pc
+     :acc acc
+     :executions executions}))
+
+(defn part1
+  [input]
+  (->> {:instructions (parse-input input)
+        :pc           0
+        :acc          0
+        :executions #{}}
+       (iterate execute)
+       (drop-while #(not (contains? (:executions %) (:pc %))))
+       first
+       :acc))
+
+(defn test-execution
+  [instructions]
+  (->> {:instructions instructions
+        :pc           0
+        :acc          0
+        :executions #{}}
+       (iterate execute)
+       (drop-while #(and
+                     (not (contains? (:executions %) (:pc %)))
+                     (not= (:pc %) (count instructions)))) ;; 명령을 끝까지 실행하면 중단
+       first))
+
+(defn switch-op
+  [m]
+  (case (:op m)
+    :jmp (assoc m :op :nop)
+    :nop (assoc m :op :jmp)))
+
+
+(defn gen-test-combinations
+  "nop -> jmp or jmp -> nop 으로 변환한 모든 조합을 구한다.
+   input: [{}{}...]
+   output: ([{}{}...],[{}{}...] ...)"
+  [instructions]
+  (let [non-acc-indexes (->> instructions
+                             (map-indexed vector)
+                             (filter (fn [[_idx item]] (not= (:op item) :acc)))
+                             (map first))]
+    (map #(assoc instructions
+                 %
+                 (switch-op (nth instructions %)))
+         non-acc-indexes)))
+
+(defn part2
+  [input]
+  (let [instructions (into [] (parse-input input))]
+    (->> (gen-test-combinations instructions)
+         (map test-execution)
+         (filter (fn [{:keys [instructions pc]}] (= pc (count instructions))))
+         first
+         :acc)))
+
+(comment
+  (part1 input)
+  (part2 input)
+
+  (parse-input input-sample)
+  (test-execution (parse-input input-sample))
+  (gen-test-combinations (into [] (parse-input input)))
+  )
